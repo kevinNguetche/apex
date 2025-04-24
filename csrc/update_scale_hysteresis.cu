0b249fad020c794b8f4aea7da2bee3c23bf072b0
kevin <etude.nguetche@gmail.com>
2025-04-23 20:10:27 -0400

    update apex/csrc/update_scale_hysteresis.cu , from mutable_data_ptr and const_data_ptr to data_ptr

--- csrc/update_scale_hysteresis.cu
+++ csrc/update_scale_hysteresis.cu
@@ -56,10 +56,10 @@ at::Tensor update_scale_hysteresis_cuda(at::Tensor current_scale,
                                         const int hysteresis)
 {
   update_scale_hysteresis_cuda_kernel<<<1, 1, 0, at::cuda::getCurrentCUDAStream()>>>(
-    current_scale.mutable_data_ptr<float>(),
-    growth_tracker.mutable_data_ptr<int>(),
-    hysteresis_tracker.mutable_data_ptr<int>(),
-    found_inf.const_data_ptr<float>(),
+    current_scale.data_ptr<float>(),
+    growth_tracker.data_ptr<int>(),
+    hysteresis_tracker.data_ptr<int>(),
+    found_inf.data_ptr<float>(),
     growth_factor,
     backoff_factor,
     growth_interval,
